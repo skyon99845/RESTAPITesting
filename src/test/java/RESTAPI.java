e14944f6059689183f9a956c1947b69bfc3eb057
skyon99845 <skyon9801@gmail.com>
2022-05-30 14:56:39 +0530

    first commit

---
+++ src/test/java/RESTAPI.java
@@ -0,0 +1,46 @@
+import io.restassured.RestAssured;
+        import io.restassured.http.Method;
+        import io.restassured.response.Response;
+        import io.restassured.specification.RequestSpecification;
+import org.junit.Test;
+
+public class RESTAPI {
+
+    @Test
+    public void GetBooksDetails() {
+        // Specify the base URL to the RESTful web service
+        RestAssured.baseURI = "https://demoqa.com/BookStore/v1/Books";
+
+        // Get the RequestSpecification of the request to be sent to the server.
+        RequestSpecification httpRequest = RestAssured.given();
+
+        // specify the method type (GET) and the parameters if any.
+        //In this case the request does not take any parameters
+        Response response = httpRequest.request(Method.GET, "");
+
+        // Print the status and message body of the response received from the server
+        System.out.println("Status received => " + response.getStatusLine());
+        System.out.println("Response=>" + response.prettyPrint());
+    }
+
+
+
+    @Test
+    public void GetWeatherDetailsCondensed()
+    {
+        // Specify the base URL to the RESTful web service
+        RestAssured.baseURI = "https://demoqa.com/BookStore/v1/Books";
+
+        // Get the RequestSpecification of the request that is to be sent
+        // to the server.
+        RequestSpecification httpRequest = RestAssured.given();
+
+        // Call RequestSpecification.get() method to get the response.
+        // Make sure you specify the resource name.
+        Response response = httpRequest.get("");
+
+        // Response.asString method will directly return the content of the body
+        // as String.
+        System.out.println("Response Body is =>  " + response.asString());
+    }
+}
